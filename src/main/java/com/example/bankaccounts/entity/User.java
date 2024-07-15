ca50a1ce964358127ec6275d043807d20965e883
Nikita0703 <nikita.noviczenko2005@gmail.com>
2024-07-15 20:34:09 +0300

    add new features

--- src/main/java/com/example/bankaccounts/entity/User.java
+++ src/main/java/com/example/bankaccounts/entity/User.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -51,7 +52,7 @@ public class User implements UserDetails {
     @JoinColumn(name = "bankAccount_id")
     private BankAccount bankAccount;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Column(updatable = false)
     private LocalDateTime birthday;
 
